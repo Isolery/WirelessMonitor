1ee14b1c3618b55dc3b6af0a65ef602b9f4c5b90
Albert <123456@qq.com>
2020-06-15 16:47:09 +0800

    添加DTU连接云服务器并下载数据存入EEPROM

--- WirelessCom_1/WirelessCom1.c
+++ WirelessCom_1/WirelessCom1.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
@@ -13,10 +14,14 @@
 #define BEGIN   1
 #define END      0
 
+#define UART2_EN         UCSR2B |= (1<< RXEN2)
+#define UART2_DEN       UCSR2B &=~(1<< RXEN2)
+
 uint8_t failue=0;
 uint8_t ledType = 0;
 
 uint16_t countRx2 = 0;
+uint16_t val_test = 0;
 
 /*标志位相关变量*/
 uint8_t flag_LedON = END;    //是否开始点亮LED
@@ -29,13 +34,17 @@ uint32_t  tLedCount= 0;    //LED计时计数器
 /*数组定义区*/
 uint8_t rxUart0[20];   //串口0数据缓存区
 uint8_t rxUart1[20];   //串口1数据缓存区
-uint8_t rxUart2[3502];
+char rxUart2[3502];   //串口2数据缓存区，因为串口2接收的数据都是字符串显示，所以用char型表示
 uint8_t FrameData[20];    //
+char temp[10];
 
 /*函数声明区*/
 void Led_Slake(void);
 void SystemInit(void);
+void DealData(const char* rxUart2, char* pcRes);
 void Timer3_Init(void);
+uint8_t DTU_Configuration(void);
+uint8_t Time_up_Return(uint16_t n);
 void Led_Display(uint8_t ledType);
 uint8_t EEPROM_Read(uint16_t Addr);
 void DealFrameData(uint8_t* p_FrameData);
@@ -44,6 +53,7 @@ void FrameProcess(uint8_t* dstArray, uint8_t type);
 void EEPROM_Successive_Write(uint16_t Addr,uint8_t Data);
 uint8_t DecodeProtocol(uint8_t* p_rxUart0, uint8_t* p_FrameData);
 
+void(*reset)(void)=0x0000;    //软复位，程序重新从头执行
 
 /**********************************************************************************************
 * 函 数 名  ：main
@@ -53,11 +63,48 @@ uint8_t DecodeProtocol(uint8_t* p_rxUart0, uint8_t* p_FrameData);
 **********************************************************************************************/
 int main(void)
 {
-	SystemInit();
-	//EEPROM_Successive_Write(0x00,0x04);    //将EEPROM前500个字节填充成0x02
-	//DTU_Configuration();
+	
+	SystemInit(); //EEPROM_Successive_Write(0x0000,0x02);    //将EEPROM前500个字节填充成0x02
+
+	DTU_Configuration();
+	_delay_ms(100);
+
+	if(failue)  //配置失败，程序重新开始
+	{
+		failue=0;
+		reset();
+	}
+	
+	while((strstr(rxUart2, "OK") == NULL))
+	{
+		USART2_TransmitString("014469014469\r\n");  //DTU先传送014469014469
+		_delay_ms(1000);
+	}
+	USART2_TransmitString("OK\r\n");  //DTU回复OK
+	memset(rxUart2, '\0', sizeof(rxUart2));
+	countRx2=0;
+	
 	while(1)
 	{
+		if((strstr(rxUart2, "@END") != NULL))
+		{
+			UART2_DEN;       //防止串口缓存数据被改变
+			
+			_delay_ms(100);    //延时很有必要，不然服务器可能接收不到
+			
+			USART2_TransmitString("OK\r\n");    //DTU回复OK
+			DealData(rxUart2, temp);       //提取数据帧
+			
+			USART2_TransmitString(rxUart2);
+			memset(rxUart2, '\0', sizeof(rxUart2));
+			countRx2 = 0;
+			
+			UART2_EN; 
+		}
+		else
+		{
+			_delay_ms(1);
+		}
 		if(flag_NewData)    //串口有新数据到来
 		{
 			//USART1_TransmitFrame(rxUart0);    //test used
@@ -71,9 +118,12 @@ int main(void)
 			}
 			flag_NewData = FALSE;
 		}
+		else
+		{
+			_delay_ms(1);
+		}
 		Led_Display(ledType);
 	}
-	return 0;
 }
 
 /**********************************************************************************************
@@ -105,6 +155,16 @@ ISR(USART0_RX_vect)
 		UCSR0A |= (1<<MPCM0);    //读取最后一个数据，置位MPCM等待下次被寻址
 		flag_NewData = TRUE;
 	}
+}
+
+/**********************************************************************************************
+* 功能说明：串口2中断接收函数 
+* 输入参数：void
+* 返 回 值 ：void
+*********************************************************************************************/
+ISR(USART2_RX_vect)
+{
+	rxUart2[countRx2++] = UDR2;
 }
 
 /**********************************************************************************************
@@ -180,7 +240,8 @@ ISR(TIMER3_OVF_vect)
 	TCNT3H=0xBC;
 	TCNT3L=0x80;
 	
-	tLimt_rxTime++;  //LED开始定时熄灭
+	tLedCount++;    //LED开始定时熄灭
+	tLimt_rxTime++;    //接收时长限制  
 	if(flag_LedON == BEGIN) tLedCount++;    //LED开始定时熄灭
 }
 
@@ -313,73 +374,137 @@ void SystemInit(void)
 	DDRA=0xff;
 	USART0_Init(115200,1);
 	USART1_Init(115200,1);
+	USART2_Init(115200);
 	Timer3_Init();
 	sei();
 }
 
 /************************************************************************************
+* 函 数 名 ：DTU_Configuration
 * 函数介绍：DTU配置函数，用来设置DTU的TCP通道以连接云服务器
 * 输入参数：无
 * 返 回 值 ：无
+************************************************************************************/
+uint8_t DTU_Configuration(void)
+{
+	const char *pdata = rxUart2;
+	const char *enter_config = "AT+ENTERCFG\r\n";
+	const char *set_ip = "AT+SET=9,121.36.107.81\r\n";
+	const char *set_port = "AT+SET=10,33\r\n";
+	const char *set_datasource = "AT+SET=12,2\r\n";
+	const char *exit_config = "AT+EXITCFG\r\n";
+	
+	memset(rxUart2, '\0', sizeof(rxUart2));  //清除缓存数组
+	countRx2=0;
+	
+	USART2_TransmitString(enter_config);    //进入配置模式    
+	tLimt_rxTime=0;
+	while((strstr(pdata, "OK") == NULL) && (Time_up_Return(300)));    //30s内未返回OK，退出等待
+	if(failue)   //任意一步配置错误直接退出，避免不必要的操作
+	{
+		return 0;
+	}
+	memset(rxUart2, '\0', sizeof(rxUart2));
+	countRx2=0;
+	//_delay_ms(100);
+	
+	USART2_TransmitString(set_ip);    //设置数据中心2的ip地址
+	tLimt_rxTime=0;
+	while((strstr(pdata, "OK") == NULL) && (Time_up_Return(100)));    //10s内未返回OK，退出等待
+	if(failue)   //任意一步配置错误直接退出，避免不必要的操作
+	{
+		return 0;
+	}
+	memset(rxUart2, '\0', sizeof(rxUart2));
+	countRx2=0;
+	//_delay_ms(100);
+	
+	USART2_TransmitString(set_port);    //设置数据中心2的端口
+	tLimt_rxTime=0;
+	while((strstr(pdata, "OK") == NULL) && (Time_up_Return(100)));    //10s内未返回OK，退出等待
+	if(failue)   //任意一步配置错误直接退出，避免不必要的操作
+	{
+		return 0;
+	}
+	memset(rxUart2, '\0', sizeof(rxUart2));
+	countRx2=0;
+	//_delay_ms(100);
+	
+	USART2_TransmitString(set_datasource);    //设置数据中心2的数据源
+	tLimt_rxTime=0;
+	while((strstr(pdata, "OK") == NULL) && (Time_up_Return(100)));    //10s内未返回OK，退出等待
+	if(failue)   //任意一步配置错误直接退出，避免不必要的操作
+	{
+		return 0;
+	}
+	memset(rxUart2, '\0', sizeof(rxUart2));
+	countRx2=0;
+	//_delay_ms(100);
+	
+	USART2_TransmitString(exit_config);    //退出配置模式
+	tLimt_rxTime=0;
+	while((strstr(pdata, "OK") == NULL) && (Time_up_Return(100)));    //10s内未返回OK，退出等待
+	if(failue)   //任意一步配置错误直接退出，避免不必要的操作
+	{
+		return 0;
+	}
+	memset(rxUart2, '\0', sizeof(rxUart2));
+	countRx2=0;
+	
+	return 1;
+}
+
+/************************************************************************************
+* 函 数 名 ：Time_up_Return
+* 函数介绍：延时函数，当延时达到n*100ms时返回0，用于退出while循环
+* 输入参数：n （延时时间：n*100ms）
+* 返 回 值 ：无
 ***********************************************************************************/
-//char DTU_Configuration(void)
-//{
-	//memset(rxUart0, '\0', sizeof(rxUart0));  //清除缓存数组
-	//countRx2=0;
-	//
-	//Uart2_TxString("AT+ENTERCFG\r\n");    //进入配置模式
-	//tLimt_rxTime=0;
-	//while((strstr(rxUart0, "OK") == NULL) && (Time_up_Return(300)));    //60s内未返回OK，退出等待
-	//if(failue)   //任意一步配置错误直接退出，避免不必要的操作
-	//{
-		//return 0;
-	//}
-	//memset(rxUart0, '\0', sizeof(rxUart0));
-	//countRx0=0;
-	//delay_ms(100);
-	//
-	//Uart0_TxString("AT+SET=9,121.36.107.81\r\n");    //设置数据中心2的ip地址
-	//tLimt_rxTime=0;
-	//while((strstr(rxUart0, "OK") == NULL) && (Time_up_Return(100)));    //10s内未返回OK，退出等待
-	//if(failue)   //任意一步配置错误直接退出，避免不必要的操作
-	//{
-		//return 0;
-	//}
-	//memset(rxUart0, '\0', sizeof(rxUart0));
-	//countRx0=0;
-	//delay_ms(100);
-	//
-	//Uart0_TxString("AT+SET=10,33\r\n");    //设置数据中心2的端口
-	//tLimt_rxTime=0;
-	//while((strstr(rxUart0, "OK") == NULL) && (Time_up_Return(100)));    //10s内未返回OK，退出等待
-	//if(failue)   //任意一步配置错误直接退出，避免不必要的操作
-	//{
-		//return 0;
-	//}
-	//memset(rxUart0, '\0', sizeof(rxUart0));
-	//countRx0=0;
-	//delay_ms(100);
-	//
-	//Uart0_TxString("AT+SET=12,2\r\n");    //设置数据中心2的数据源
-	//tLimt_rxTime=0;
-	//while((strstr(rxUart0, "OK") == NULL) && (Time_up_Return(100)));    //10s内未返回OK，退出等待
-	//if(failue)   //任意一步配置错误直接退出，避免不必要的操作
-	//{
-		//return 0;
-	//}
-	//memset(rxUart0, '\0', sizeof(rxUart0));
-	//countRx0=0;
-	//delay_ms(100);
-	//
-	//Uart0_TxString("AT+EXITCFG\r\n");    //退出配置模式
-	//tLimt_rxTime=0;
-	//while((strstr(rxUart0, "OK") == NULL) && (Time_up_Return(100)));    //10s内未返回OK，退出等待
-	//if(failue)   //任意一步配置错误直接退出，避免不必要的操作
-	//{
-		//return 0;
-	//}
-	//memset(rxUart0, '\0', sizeof(rxUart0));
-	//countRx0=0;
-	//
-	//return 1;
-//}
+uint8_t Time_up_Return(uint16_t n)
+{
+	if(tLimt_rxTime == n)
+	{
+		failue = 1;
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+/************************************************************************************
+* 函 数 名 ：DealData
+* 函数介绍：处理从云服务器传送的数据信息，并存到EEPROM中
+* 输入参数：rxUart2
+* 返 回 值 ：无
+***********************************************************************************/
+void DealData(const char* rxUart2, char* pcRes)
+{
+	const char* p = rxUart2;
+	const char* pcBegin = NULL;
+	const char* pcEnd = NULL;
+	uint16_t val = 0;
+	uint16_t val_4 = 0, val_1 = 0;
+	
+	while(*p != '@')  //寻址到@说明后面没有数据了
+	{
+		pcBegin = strstr(p, "$");
+		pcEnd = strstr(p, "#");
+		if (pcBegin == NULL || pcEnd == NULL || pcBegin > pcEnd)
+		{
+			//printf("mail name not found!\n");
+			return;
+		}
+		else
+		{
+			pcBegin += 1;
+			p = pcEnd + 1;
+			memcpy(pcRes, pcBegin, pcEnd - pcBegin);
+			val = atoi(pcRes);    //字符串转成整数
+			val_4 = val/10;    //前三位--> 存入EEPROM中的偏移地址
+			val_1 = val%10;    //最后一位--> 灯信号
+			EEPROM_Write(0x0000+val_4, val_1);    //
+		}
+	}
+}
